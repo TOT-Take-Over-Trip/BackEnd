1e20b7d4a0164ea26368c888701f1094df0b6d2f
이동열 <comeme0101@naver.com>
2024-04-26 12:23:44 +0900

    config: add component scan

--- tot/src/main/java/com/trip/tot/TotApplication.java
+++ tot/src/main/java/com/trip/tot/TotApplication.java
@@ -2,8 +2,10 @@ package com.trip.tot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.trip")
 public class TotApplication {
 
 	public static void main(String[] args) {
