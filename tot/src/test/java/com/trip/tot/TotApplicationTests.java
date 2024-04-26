f7fa53409ef0825144577539b46e151e38063688
이동열 <comeme0101@naver.com>
2024-04-26 11:53:37 +0900

    init: create project

---
+++ tot/src/test/java/com/trip/tot/TotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.tot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
