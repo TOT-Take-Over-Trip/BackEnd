631f067b659ba29d0ff8cfb66c4c77bc87fa1078
고관우 <kwko97@gmail.com>
2024-04-26 11:39:32 +0900

    init: project create

---
+++ tot/tot/src/main/java/com/trip/tot/TotApplication.java
@@ -0,0 +1,13 @@
+package com.trip.tot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotApplication.class, args);
+	}
+
+}
