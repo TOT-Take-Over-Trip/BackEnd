a8eea34d745a843096496b7600f362e141b3e23c
고관우 <kwko97@hanyang.ac.kr>
2024-05-07 00:13:51 +0900

    feat: 좀 더 생각하고 짜야할듯 url mapping 관련

--- tot/src/main/java/com/trip/course/controller/CourseController.java
+++ tot/src/main/java/com/trip/course/controller/CourseController.java
@@ -1,6 +1,8 @@
 package com.trip.course.controller;
 
 import com.trip.course.model.CourseDto;
+import com.trip.course.service.CourseService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -8,37 +10,41 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/courses")
+@RequiredArgsConstructor
 public class CourseController {
+
+    private final CourseService courseService;
 
     // TODO: 구현해야 함
     @GetMapping
     public List<CourseDto> getCourses() {
-        return new ArrayList<CourseDto>();
+        return courseService.selectAllCourses();
     }
 
     // TODO: 구현해야 함
     @PostMapping
     public void addCourse(@RequestBody CourseDto course) {
-
+        courseService.insertCourse(course);
     }
 
     // TODO: 구현해야 함
     @GetMapping("/{courseId}")
     public CourseDto getCourse(@PathVariable(value = "courseId") Long courseId) {
-        return new CourseDto();
+        return courseService.selectCourseById(courseId);
     }
 
 
     // TODO: 구현해야 함
     @PutMapping("/{courseId}")
     public void updateCourse(@PathVariable(value = "courseId") Long courseId, @RequestBody CourseDto course) {
-
+        courseService.modifyCourse(course);
     }
 
     // TODO: 구현해야 함
+    // 이거 memberId도 필요한듯
     @PostMapping("/takeover/{courseId}")
     public void takeover(@PathVariable(value = "courseId") Long courseId) {
-
+//        courseService.takeOverCourse();
     }
 
     // TODO: 구현해야 함
