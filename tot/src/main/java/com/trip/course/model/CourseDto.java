5d1b8058d641c7c32c853159af1dd57520c8e7d2
고관우 <kwko97@hanyang.ac.kr>
2024-05-06 23:58:25 +0900

    feat: CourseDto

--- tot/src/main/java/com/trip/course/model/CourseDto.java
+++ tot/src/main/java/com/trip/course/model/CourseDto.java
@@ -1,4 +1,34 @@
 package com.trip.course.model;
 
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
 public class CourseDto {
+
+    private Long courseId;
+    private Long memberId;
+    private String title;
+    private String content;
+    private Long hit;
+    private String createdDate;
+    private String updatedDate;
+    private String status;
+
+
+    public CourseDto() {
+    }
+
+    @Builder
+    public CourseDto(Long courseId, Long memberId, String title, String content, Long hit, String createdDate, String updatedDate, String status) {
+        this.courseId = courseId;
+        this.memberId = memberId;
+        this.title = title;
+        this.content = content;
+        this.hit = hit;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.status = status;
+    }
 }
